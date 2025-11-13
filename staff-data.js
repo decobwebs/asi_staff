6f06045ef8c2eeb38ef00def4a4e99d6cbcb876d
decobwebs <cobwebb784@gmail.com>
2025-11-14 00:47:17 +0100

    first commit

---
+++ staff-data.js
@@ -0,0 +1,44 @@
+const staffDatabase = {
+  "Ais/2022/0012": {
+    name: "Bashir Balanfalke Yusuf",
+    role: "Operations",
+    status: "Active",
+    image: "images/0012.png",
+  },
+  "Ais/2021/0011": {
+    name: "Yusuf Abdulqudus Adisa",
+    role: "Operations",
+    status: "Active",
+    image: "images/0011.png",
+  },
+  "Ais/2022/0015": {
+    name: "Godowoli Abubakar Haruna",
+    role: "Director",
+    status: "Active",
+    image: "images/0015.png",
+  },
+  "Ais/2022/001": {
+    name: "Auwalu Ibrahim Balanfalke",
+    role: "Chief Operations Officer",
+    status: "Active",
+    image: "images/001.png",
+  },
+  "Ais/2020/0010": {
+    name: "La'aro Abdulrazaak Omotunde",
+    role: "Operations Manager",
+    status: "Active",
+    image: "images/0010.png",
+  },
+  "Ais/2022/0014": {
+    name: "Khalifa Ibrahim",
+    role: "Chief Accountant",
+    status: "Active",
+    image: "images/0014.png",
+  },
+  "Ais/2022/0013": {
+    name: "Muhammed Lawan",
+    role: "Operations",
+    status: "Active",
+    image: "images/0013.png",
+  },
+};
