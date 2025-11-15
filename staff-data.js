de8f5c3291982585066f0891a6a8e29cce624725
decobwebs <cobwebb784@gmail.com>
2025-11-15 08:40:42 +0100

    Updated AISL

--- staff-data.js
+++ staff-data.js
@@ -1,41 +1,41 @@
 const staffDatabase = {
-  "Ais/2022/0012": {
+  "AISL/2022/0012": {
     name: "Bashir Balanfalke Yusuf",
     role: "Operations",
     status: "Active",
     image: "images/0012.png",
   },
-  "Ais/2021/0011": {
+  "AISL/2021/0011": {
     name: "Yusuf Abdulqudus Adisa",
     role: "Operations",
     status: "Active",
     image: "images/0011.png",
   },
-  "Ais/2022/0015": {
+  "AISL/2022/0015": {
     name: "Godowoli Abubakar Haruna",
     role: "Director",
     status: "Active",
     image: "images/0015.png",
   },
-  "Ais/2022/001": {
+  "AISL/2022/001": {
     name: "Auwalu Ibrahim Balanfalke",
     role: "Chief Operations Officer",
     status: "Active",
     image: "images/001.png",
   },
-  "Ais/2020/0010": {
+  "AISL/2020/0010": {
     name: "La'aro Abdulrazaak Omotunde",
     role: "Operations Manager",
     status: "Active",
     image: "images/0010.png",
   },
-  "Ais/2022/0014": {
+  "AISL/2022/0014": {
     name: "Khalifa Ibrahim",
     role: "Chief Accountant",
     status: "Active",
     image: "images/0014.png",
   },
-  "Ais/2022/0013": {
+  "AISL/2022/0013": {
     name: "Muhammed Lawan",
     role: "Operations",
     status: "Active",
